bcaeb970ecdbc257928de8ce68267ba8f5be97c7
bmasseybrannen <brannenben2@gmail.com>
2021-02-06 12:49:07 -0500

    Update folder_creator.py

--- folder_creator.py
+++ folder_creator.py
@@ -3,12 +3,12 @@ import os
 print("Enter your folder name to add to documents: ")
 newDir = input()
 print("Enter the number of chapters + 1 (16 chapters would be 17): ")
-y = input()
-y = int(y)
+ch_Num = input()
+ch_Num = int(y)
 
 
-dirs = range(1, y)
-parent_dir = f"/Users/benjaminbrannen/Documents/{newDir}"
+dirs = range(1, ch_Num)
+parent_dir = f"/Documents/{newDir}"
 
 for dir in dirs:
     new_dir = ('Chapter' + str(dir))
